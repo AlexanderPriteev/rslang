dcc0cbc083ad06b5d12464de3275146bfec40fb3
Alexander Priteev <doggars@mail.ru>
2022-08-21 22:40:01 +0600

    feat: add markup for statistics

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,14 @@
 import '../styles.scss';
 import { pageRender } from '../page/page';
 
+import {statisticsRender} from "../staistics/staistics";
+
 //должны быть последними
 import '../assets/styles/atom.scss';
 
 const app = () => {
-  pageRender();
+ // pageRender();
+    statisticsRender()
 };
 
 export default app;
