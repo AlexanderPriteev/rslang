0f520bce2fe76f8f5d4fbb1d4c0c323e2865f6c5
Yura Lapitski <lyurik@tut.by>
2022-08-28 21:12:56 +0300

    feta: add start game from books

--- src/textbook/textbook.ts
+++ src/textbook/textbook.ts
@@ -2,15 +2,12 @@ import renderTextbookTemplate from './renderTextbookTemplate';
 import pageSwitcher from './pageSwitcher';
 import './textbook.scss';
 import './word.scss';
-import requestMethods from "../services/requestMethods";
-import {clearUserStore, getStore} from "../storage";
+import requestMethods from '../services/requestMethods';
+import { clearUserStore, getStore } from '../storage';
 
-const  textbook = async () => {
-  const user = getStore()
-  if(user)
-  await requestMethods()
-      .getAllUserWords(user.id, user.token)
-      .catch(clearUserStore);
+const textbook = async () => {
+  const user = getStore();
+  if (user) await requestMethods().getAllUserWords(user.id, user.token).catch(clearUserStore);
   renderTextbookTemplate('.content-wrapper');
   pageSwitcher();
 };
