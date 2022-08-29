7f0184b632485cf7904986e9dda25f43c76be8c5
Yura Lapitski <lyurik@tut.by>
2022-08-29 11:40:41 +0300

    feat: edit textbook.ts after merge

--- src/textbook/textbook.ts
+++ src/textbook/textbook.ts
@@ -5,28 +5,11 @@ import './word.scss';
 import requestMethods from '../services/requestMethods';
 import { clearUserStore, getStore } from '../storage';
 
-<<<<<<< HEAD
 const textbook = async () => {
   const user = getStore();
   if (user) await requestMethods().getAllUserWords(user.id, user.token).catch(clearUserStore);
   renderTextbookTemplate('.content-wrapper');
   pageSwitcher();
-=======
-const textbook = () => {
-  const user = getStore();
-  if (user)
-    void requestMethods()
-      .getAllUserWords(user.id, user.token)
-      .then(() => {
-        renderTextbookTemplate('.content-wrapper');
-        pageSwitcher();
-      })
-      .catch(clearUserStore);
-  else {
-    renderTextbookTemplate('.content-wrapper');
-    pageSwitcher();
-  }
->>>>>>> 50460971a0611c8cf0e7ae14d33809265aa1fba0
 };
 
 export default textbook;
