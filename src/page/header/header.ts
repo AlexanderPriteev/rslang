5e829c23639989a5c48c703b6769dac7f46cb858
priteev <garsteera@gmail.com>
2022-08-30 12:13:28 +0600

    feat: add theme switcher

--- src/page/header/header.ts
+++ src/page/header/header.ts
@@ -2,6 +2,21 @@ import createElement from '../../helpers/createElement';
 import { DevsLinks } from '../about/about-interface';
 import { headerAuth } from './header-auth';
 
+function themeSwitch(){
+  const logo = document.querySelector('.header__logo img') as HTMLImageElement;
+  const body = document.body
+  const isDark = document.body.classList.contains('dark-theme')
+  if(isDark){
+    body.classList.remove('dark-theme')
+    localStorage.removeItem('isDarkTheme')
+  }
+  else{
+    body.classList.add('dark-theme')
+    localStorage.setItem('isDarkTheme', '1')
+  }
+  logo.src = `./assets/images/lang-logo${!isDark ? '-dark' : ''}.svg`
+}
+
 export function headerRender(userHead?: boolean) {
   const darkTheme = document.body.classList.contains('dark-theme')
   const logo: DevsLinks = {
@@ -16,10 +31,14 @@ export function headerRender(userHead?: boolean) {
   logoHeader.setAttribute('href', logo.link);
   header.append(logoHeader);
 
+  const themeSwitcherText = `<i class="theme-switch__night"></i><i class="theme-switch__day"></i>`
+  const themeSwitcher = createElement('div', ['theme-switch'], themeSwitcherText)
+  const headerUser = createElement('div', ['header__user']);
+  themeSwitcher.onclick = () => themeSwitch();
+  headerUser.append(themeSwitcher)
   if (userHead) {
-    const headerUser = createElement('div', ['header__user']);
     headerUser.append(headerAuth());
-    header.append(headerUser);
   }
+  header.append(headerUser);
   return header;
 }
