3994fa869cdde48828048bdd0939f2e8ffc2ac76
Alexander Priteev <doggars@mail.ru>
2022-08-30 20:38:32 +0600

    docs: about team

--- src/page/about/about-const.ts
+++ src/page/about/about-const.ts
@@ -6,18 +6,20 @@ export const devs: DevsLinks[] = [
     link: 'https://github.com/alexanderpriteev',
     image: 'assets/images/devs/alexander.jpg',
     nameRu: 'Александр',
-    about: 'Что-то сделал...',
-  },
-  {
-    name: 'Yury Lapitski',
-    link: 'https://github.com/yura703',
-    image: 'assets/images/devs/Yura.jpg',
-    nameRu: 'Юрий',
+    about: 'Сделал UI/UX дизайн, адаптивную верстку. Реализовал роутинг, статистику,  логирование и отображения прогресса изучения слов, настройку профиля.',
   },
   {
     name: 'Kirill Kolupaev',
     link: 'https://github.com/aoiharu1',
     image: 'assets/images/devs/AoiHaru1.jpg',
     nameRu: 'Кирилл',
+    about: 'Организовал работу с API. Реализовал страницы "Учебник" и "Словарь".'
+  },
+  {
+    name: 'Yury Lapitski',
+    link: 'https://github.com/yura703',
+    image: 'assets/images/devs/Yura.jpg',
+    nameRu: 'Юрий',
+    about: 'Team Lead. Реализовал авторизацию. Сделал игры "Аудиовызов" и "Спринт".'
   },
 ];
